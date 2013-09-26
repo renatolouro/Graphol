0437e47b06e240c32b83cf62f894d318828210d1
Renato Louro <renato@silostecnologia.com.br>
2013-09-26 20:05:43 -0300

    Tratamento msg async pelo nodo

---
+++ PrototipoJS/vm/graphol/lang/message/async.js
@@ -0,0 +1,18 @@
+function Async() {
+    var p_mytype = "message";
+
+    this.receive = function(pValue) {
+
+    }
+
+    this.exec = function() {
+    }
+
+    this.getType = function() {
+        return p_mytype;
+    }
+    
+        this.getMessage = function() {
+            return "async";
+        }
+}
